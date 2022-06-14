8b2ead7a70adeb0dbcaaf14085b916279dd635fc
littlephillips <phillipkinuthia72@gmail.com>
2022-06-15 00:01:45 +0300

    added a to-do list and a delete button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  let listForm =  document.querySelector('form')
+   listForm.addEventListener('submit', (e) => {
+       e.preventDefault()
+       toDo(e.target.new.value)
+       listForm.reset()
+ })
+ 
+ });
+ 
+ // function creating a do list/ delete in the dom and appending them
+ function toDo(toDoList){
+   let li = document.createElement('li')
+   li.textContent = `${toDoList} `
+   document.querySelector('#tasks').appendChild(li)
+ 
+   //create the delete element and append it
+   let btn = document.createElement('button')
+   btn.textContent = ' X'
+   li.appendChild(btn)
+ 
+     //delete event
+     btn.addEventListener('click', deleteList)
+ 
+ }
+ 
+ function deleteList (e){
+   e.target.parentNode.remove()
+ }
+ 
